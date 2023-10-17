21eb6a0b4e524b5afc2c7979405d2aa0b24c30ea
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-10-18 04:24:29 +0600

    add input element and confiem

--- script.js
+++ script.js
@@ -10,11 +10,37 @@ const dummyTransactions = [
   { Id: 1, text: "Flower", amount: -20 },
   { Id: 2, text: "Salary", amount: 300 },
   { Id: 3, text: "Book", amount: -10 },
-  { Id: 4, text: "Camera", amount: 150 }
+  { Id: 4, text: "Camera", amount: 150 },
 ];
 let transactions = dummyTransactions;
 
-// add transactions to DOM list on js section..add s
+// add transaction
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value.trim() === '' || amount.value.trim() === "") {
+    alert("Pleas add text and amount");
+  } else {
+    const transaction = {
+      Id: generateID(),
+      text: text.value,
+      amount: amount.value
+    };
+    transactions.push(transaction);
+
+    addTransactionDOM(transaction);
+
+    updateValues();
+
+    text.value = '';
+    amount.value = '';
+  }
+}
+
+// generate random Id
+function getElementById() {
+  return Math.floor(Math.random() * 100000000);
+}
 
 function addTransactionDOM(transaction) {
   // get sign on this function
@@ -33,31 +59,34 @@ function addTransactionDOM(transaction) {
 }
 
 // Update the balance, income amd expense
-function updateValues(){
-  const amounts = transactions.map(transaction => transaction.amount);
+function updateValues() {
+  const amounts = transactions.map((transaction) => transaction.amount);
 
-  const total = amounts.reduce((acc, item) => (acc += item),0).toFixed(2); 
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
   const income = amounts
-    .filter(item => item > 0)
+    .filter((item) => item > 0)
     .reduce((acc, item) => (acc += item), 0)
     .toFixed(2);
 
-    const expense = 
-    (amounts.filter  (item => item < 0).reduce((acc, item) =>(acc += item), 0) * -1)
-    .toFixed(2);
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
 
-    balance.innerText = `$${total}`;
-    money_plus.innerText = `$${income}`;
-    money_minus.innerText = `$${expense}`;
+  balance.innerText = `$${total}`;
+  money_plus.innerText = `$${income}`;
+  money_minus.innerText = `$${expense}`;
 }
 
-// init app 
-function init(){
-  list.innerHTML = '';
+// init app
+function init() {
+  list.innerHTML = "";
 
   transactions.forEach(addTransactionDOM);
   updateValues();
 }
 
 init();
+
+form.addEventListener("submit", addTransaction);
