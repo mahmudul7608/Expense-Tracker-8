b93f7a3c96df058f4aebcfdd55a27bb36c4aee49
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-10-18 03:56:37 +0600

    setup history section

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ const dummyTransactions = [
   { Id: 1, text: "Flower", amount: -20 },
   { Id: 2, text: "Salary", amount: 300 },
   { Id: 3, text: "Book", amount: -10 },
-  { Id: 4, text: "Camera", amount: 150 },
+  { Id: 4, text: "Camera", amount: 150 }
 ];
 let transactions = dummyTransactions;
 
 // add transactions to DOM list on js section..add s
 
-function addTransactionDOM(transactions) {
+function addTransactionDOM(transaction) {
   // get sign on this function
   const sign = transaction.amount < 0 ? "-" : "+";
 
@@ -32,11 +32,32 @@ function addTransactionDOM(transactions) {
   list.appendChild(item);
 }
 
+// Update the balance, income amd expense
+function updateValues(){
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item),0).toFixed(2); 
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+    const expense = 
+    (amounts.filter  (item => item < 0).reduce((acc, item) =>(acc += item), 0) * -1)
+    .toFixed(2);
+
+    balance.innerText = `$${total}`;
+    money_plus.innerText = `$${income}`;
+    money_minus.innerText = `$${expense}`;
+}
+
 // init app 
 function init(){
   list.innerHTML = '';
 
   transactions.forEach(addTransactionDOM);
+  updateValues();
 }
 
 init();
